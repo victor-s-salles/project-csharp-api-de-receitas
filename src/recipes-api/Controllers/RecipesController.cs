2934362b88a02769f1ae1b509667182fc9a80f0f
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-16 20:12:39 -0300

    Feat: Requisito 4

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -53,7 +53,16 @@ public class RecipesController : ControllerBase
     [HttpPut("{name}")]
     public IActionResult Update(string name, [FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _service.RecipeExists(name);
+            _service.UpdateRecipe(recipe);
+            return NoContent();
+        }
+        catch
+        {
+            return BadRequest();
+        }
     }
 
     // 5 - Sua aplicação deve ter o endpoint DEL /recipe
